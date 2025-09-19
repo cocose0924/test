28f10f15c5a5d5ab6de485b551154461eee10313
cocose0924 <cocose.justice@gmail.com>
2025-09-19 19:22:55 +0900

    new

--- middleware.js
+++ middleware.js
@@ -2,13 +2,13 @@ import { jwtVerify } from "jose"
 import {NextResponse} from "next/server"
 
 export async function middleware(request){
-    const token=await request.headers.get("Authorization")?.split("")[1]
+    const token=await request.headers.get("Authorization")?.split(" ")[1]
 
     if(!token){
         return NextResponse.json({message:"トークンがありません"})
     }
     try{
-        const secretKey=new TextEncoder().encode("next-market-route-handlers")
+        const secretKey=new TextEncoder().encode("next-market-route-handles")
         const decodedJwt=await jwtVerify(token,secretKey)
         console.log("decodedJwt",decodedJwt)
         return NextResponse.next()
